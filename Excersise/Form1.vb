f31b5badb5ea297e27e70695c81a4ce06fd01906
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-04 11:28:04 -0500

    Commit

--- Excersise/Form1.vb
+++ Excersise/Form1.vb
@@ -1,3 +1,42 @@
 ﻿Public Class frmMain
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
+        'Closes the application:
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+
+        'Constants:
+        Const STAIR_STEPPER As Double = 446
+        Const ELLIPTICAL_TRAINER As Double = 670
+        Const STATIONARY_BICYCLE As Double = 520
+        Const CALORIES_NEEDED_TO_BURN_FOR_ONE_POUND As Double = 3500
+
+        'Variables
+        Dim dblStepper As Double
+        Dim dblTrainer As Double
+        Dim dblBicycle As Double
+        Dim dblStepperTotal As Double
+        Dim dblTrainerTotal As Double
+        Dim dblBicycleTotal As Double
+        Dim dblCaloriesBurnedTotal As Double
+
+        'Assigns hours on each machine into variables from text boxes:
+        Double.TryParse(txtStepper.Text, dblStepper)
+        Double.TryParse(txtTrainer.Text, dblTrainer)
+        Double.TryParse(txtBicycle.Text, dblBicycle)
+
+        'Calculates, put total calories burned on each machine into a variable:
+        dblStepperTotal = dblStepper * STAIR_STEPPER
+        dblTrainerTotal = dblTrainer * ELLIPTICAL_TRAINER
+        dblBicycleTotal = dblBicycle * STATIONARY_BICYCLE
+
+        'Calculates total calories burned by adding above machine totals:
+        dblCaloriesBurnedTotal = dblStepperTotal + dblTrainerTotal + dblBicycleTotal
+
+        'Calculates
+
+    End Sub
 End Class
