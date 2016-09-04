d1e38bdc77c14b563c6ffccbffd7c95542f49973
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-04 11:07:05 -0500

    First Commit

--- Excersise/My Project/Application.Designer.vb
+++ Excersise/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Excersise.Form1
+            Me.MainForm = Global.Excersise.frmMain
         End Sub
     End Class
 End Namespace
